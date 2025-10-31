31a1493b25b043ffef7b6b25d4babd26c265fcc1
fernando sena <fernandopadre24@gmail.com>
2025-10-31 13:17:39 +0000

    FirebaseError

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyA_Z-a_Y-a_Y-a_Y-a_Y-a_Y-a_Y-a",
-  authDomain: "credfacil-fb.firebaseapp.com",
-  projectId: "credfacil-fb",
-  storageBucket: "credfacil-fb.appspot.com",
-  messagingSenderId: "331294833292",
-  appId: "1:331294833292:web:6e18f26a7f9a2b163d4a4e",
-  measurementId: "G-799J0J2Q28"
+  apiKey: "AIzaSyCV3Cda_5Yd_e3h-c6vFw4k-e-a-k-e",
+  authDomain: "dev-credfacil.firebaseapp.com",
+  projectId: "dev-credfacil",
+  storageBucket: "dev-credfacil.appspot.com",
+  messagingSenderId: "1059885834896",
+  appId: "1:1059885834896:web:1e4a2c5a0b7c3d2e1f3b0a",
+  measurementId: "G-FKE1B8H21F"
 };
