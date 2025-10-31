4a2da4b191a7afab8c91c27ebcc429c031155aaa
fernando sena <fernandopadre24@gmail.com>
2025-10-31 15:54:56 +0000

    Try fixing this error: `Console Error: [2025-10-31T15:54:32.376Z] @fireb

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -32,17 +32,6 @@ function initializeFirebase(options: FirebaseOptions = firebaseConfig): {
   const auth = getAuth(app);
   const firestore = getFirestore(app);
 
-  if (process.env.NODE_ENV === "development") {
-    // Point to the emulators running on localhost.
-    try {
-      connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
-      connectFirestoreEmulator(firestore, "127.0.0.1", 8080);
-    } catch (e) {
-      console.error("Error connecting to Firebase emulators. Is it running?", e);
-    }
-  }
-
-
   return { app, auth, firestore };
 }
 
