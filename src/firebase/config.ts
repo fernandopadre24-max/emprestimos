1f261e237f71b5f03b6fdd1745d04279d186a5c0
fernando sena <fernandopadre24@gmail.com>
2025-10-31 14:01:00 +0000

    FirebaseError ainda

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyBF-GqIN9aR3aL_fB3GqSOqu5p_ZqY-43o",
-  authDomain: "credfacil-app.firebaseapp.com",
-  projectId: "credfacil-app",
-  storageBucket: "credfacil-app.appspot.com",
-  messagingSenderId: "33890251390",
-  appId: "1:33890251390:web:44c5a716241a7e4b9319e3",
-  measurementId: "G-L0B22L3N5W"
+  apiKey: "AIzaSyC1kPq1oG-kM8dZ5-1yJ1kX3sJ4eN2b8M",
+  authDomain: "dev-prototyper-project.firebaseapp.com",
+  projectId: "dev-prototyper-project",
+  storageBucket: "dev-prototyper-project.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:a1b2c3d4e5f6a7b8c9d0e1"
 };
