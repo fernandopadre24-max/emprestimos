894a27e07810558821f0649985c8244ee3b5ade9
fernando sena <fernandopadre24@gmail.com>
2025-10-31 12:51:45 +0000

    erro ainda

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,11 +2,11 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyDP_Or89y_3y8xWkE4d_5k8x-Z8c8Y4J2c",
+  authDomain: "credfacil-f851c.firebaseapp.com",
+  projectId: "credfacil-f851c",
+  storageBucket: "credfacil-f851c.appspot.com",
+  messagingSenderId: "105658145244",
+  appId: "1:105658145244:web:127fd742c3c6f8903c734b",
+  measurementId: "G-J51J3RV4S4"
 };
