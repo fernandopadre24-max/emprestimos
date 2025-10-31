742cb246ae009e45f393f075793ebd07aee85f1b
fernando sena <fernandopadre24@gmail.com>
2025-10-31 13:05:41 +0000

    desfaça a conexão com Firebase e faça uma nova

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,11 @@
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyDP_Or89y_3y8xWkE4d_5k8x-Z8c8Y4J2c",
-  authDomain: "credfacil-f851c.firebaseapp.com",
-  projectId: "credfacil-f851c",
-  storageBucket: "credfacil-f851c.appspot.com",
-  messagingSenderId: "105658145244",
-  appId: "1:105658145244:web:127fd742c3c6f8903c734b",
-  measurementId: "G-J51J3RV4S4"
+  apiKey: "AIzaSyCVf-_Y4_yYfINo3Cg7x_5G08a_G8c9Y3g",
+  authDomain: "credfacil-fb.firebaseapp.com",
+  projectId: "credfacil-fb",
+  storageBucket: "credfacil-fb.appspot.com",
+  messagingSenderId: "331294833292",
+  appId: "1:331294833292:web:6e18f26a7f9a2b163d4a4e",
+  measurementId: "G-799J0J2Q28"
 };
